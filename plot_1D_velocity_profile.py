Fixed major error in velocity calculation; fixed boundary conditions

plot_1D_velocity_profile.py
@@ -6,18 +6,17 @@ nu = 4.0 / 1400 # wikipedia: mu = 2000 - 10000 mPas; density = 1.34 - 1.45 kg/l
 omega = 2 * np.pi * 1.0
 r0 = 0.5e-2
 v0 = omega * r0
-R0 = 1.8 * r0
+R0 = 1.6 * r0
 
 phi = 0
-r = np.linspace(r0, 3*r0, 300)
-
+r = np.linspace(r0, R0, 300)
 
 
 plt.close("all")
-A = 1.3
 for phi in np.array([0, 90, 180]) * np.pi/180:
     v = get_velocity(r, phi, v0, r0, nu, R0)
     plt.plot(r*1e3, v*1e3, label=f"$\\varphi$={phi*180/np.pi:.0f}")
+plt.plot([r0*1e3, R0*1e3], [v0*1e3, v0*R0/r0*1e3], ls='--', color='gray')
 plt.grid(alpha=0.5)
 plt.ylim([-100,200])
 plt.axhline(0, color='k', lw=2)

plot_2D_velocity_profile.py
@@ -1,41 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches
-from solver import get_velocity_wrong, get_R_outer
+from solver import get_velocity, get_R_outer
 
-omega = 2 * np.pi * 1.0
+omega = 2 * np.pi * 1
 r0 = 0.5e-2
 v0 = omega * r0
-nu = 4.0 / 1400 # wikipedia: mu = 2000 - 10000 mPas; density = 1.34 - 1.45 kg/l
-R0 = 1.9 * r0
+nu = 6.0 / 1400 # wikipedia: mu = 2000 - 10000 mPas; density = 1.34 - 1.45 kg/l
+R0 = 1.449 * r0
+# R0 = 1.44 * r0
 
-phi_values_outer = np.linspace(0, 2*np.pi, 200)
+n_outer = 500
+phi_values_outer = np.linspace(0, 2*np.pi, n_outer)
 R_outer = get_R_outer(phi_values_outer, v0, r0, nu, R0)
 x_outer, y_outer = R_outer * np.cos(phi_values_outer), R_outer * np.sin(phi_values_outer)
 
 plt.close("all")
-fig, ax = plt.subplots(1,1, figsize=(14,8), dpi=100)
-ax.add_patch(plt.Circle((0, 0), r0 * 1e3, color='gray'))
-ax.add_patch(matplotlib.patches.FancyArrowPatch((1e3*r0/2, 0), (0, 1e3*r0/2),
+fig, axes = plt.subplots(1,2, figsize=(13,6), dpi=100)
+axes[0].add_patch(plt.Polygon(1e3*np.array([x_outer, y_outer]).T, color='gold', alpha=0.3, zorder=-1))
+axes[0].add_patch(plt.Circle((0, 0), r0 * 1e3, color='gray'))
+axes[0].add_patch(matplotlib.patches.FancyArrowPatch((1e3*r0/2, 0), (0, 1e3*r0/2),
                              connectionstyle=f"arc3,rad={r0*1e3/8:f}", color='k',
                                                      arrowstyle="Simple, tail_width=0.5, head_width=4, head_length=8"))
 
-ax.plot(x_outer*1e3, y_outer*1e3, lw=3, color='gold')
-ax.axis("equal")
-ax.grid(alpha=0.5, zorder=-1)
+axes[0].plot(x_outer*1e3, y_outer*1e3, lw=3, color='gold')
+axes[0].axis("equal")
+axes[0].set_xlabel("x / mm")
+axes[0].set_xlabel("y / mm")
+axes[0].set_title("Flow profile")
+
+
+phi_grid = np.linspace(0, 2*np.pi, 200)
+for r_grid in np.arange(1, 10):
+    axes[0].plot(r_grid * np.cos(phi_grid), r_grid * np.sin(phi_grid), color='gray', alpha=0.1)
 
 n_phi_quiver = 16
 n_r_quiver = 10
-phi_quiver = (np.arange(n_phi_quiver) * 2 * np.pi / n_phi_quiver)[None, :]
-r_quiver = np.linspace(r0, np.max(R_outer), n_r_quiver)[:, None]
-R_outer_quiver = get_R_outer(phi_quiver.flatten(), v0, r0, nu, R0)[None, :]
-
-v_quiver = get_velocity_wrong(r_quiver, phi_quiver, v0, r0, nu)
-v_quiver[r_quiver > R_outer_quiver] = np.nan
+phi_quiver = (np.arange(n_phi_quiver) * 2 * np.pi / n_phi_quiver)
+R_outer_quiver = get_R_outer(phi_quiver.flatten(), v0, r0, nu, R0)
+r_quiver = np.linspace(r0, np.max(R_outer), n_r_quiver)
+v_quiver = [get_velocity(r_quiver, phi, v0, r0, nu, R) for phi, R in zip(phi_quiver, R_outer_quiver)]
+v_quiver = np.array(v_quiver).T
+v_quiver[r_quiver[:,None] > R_outer_quiver[None, :]] = np.nan
 vx_quiver = -np.sin(phi_quiver) * v_quiver
 vy_quiver = np.cos(phi_quiver) * v_quiver
-x_quiver, y_quiver = r_quiver * np.cos(phi_quiver), r_quiver * np.sin(phi_quiver)
-ax.quiver(x_quiver*1e3, y_quiver*1e3, vx_quiver*1e3, vy_quiver*1e3, scale=1e3, headwidth=3, headlength=4, width=3e-3, color='brown')
+x_quiver, y_quiver = r_quiver[:,None] * np.cos(phi_quiver), r_quiver[:,None] * np.sin(phi_quiver)
+axes[0].quiver(x_quiver*1e3, y_quiver*1e3, vx_quiver*1e3, vy_quiver*1e3, scale=0.7e3, headwidth=3, headlength=4, width=5e-3, color='brown')
 
+for i, ind in enumerate([0, 4, 8, 12]):
+    r = np.linspace(r0, R_outer_quiver[ind], 100)
+    v = get_velocity(r, phi_quiver[ind], v0, r0, nu, R_outer_quiver[ind])
+    ls = '--' if i  == 3 else '-'
+    axes[1].plot(r*1e3, v*1e3, label=f"$\\varphi$ = {phi_quiver[ind]*180/np.pi:.0f}°", ls=ls)
+axes[1].set_ylim([0, None])
+axes[1].grid(alpha=0.5)
+axes[1].set_xlabel("r [mm]")
+axes[1].set_ylabel("v [mm/s]")
+axes[1].set_title("Velocity")
+axes[1].legend()
 
+fig.tight_layout()
+fig.savefig("/home/mscherbela/develop/iypt_saving_honey/plots/flow_profile.png", dpi=400, bbox_inches='tight')
 

solver.py
@@ -3,27 +3,33 @@ from scipy.optimize import root
 
 g = 9.81
 
-def get_velocity_wrong(r, phi, v0, r0, nu):
-    """Calculate the velocity profile, i.e. the flow velocity in tangential direction for an arbitrary r and phi.
-    i.e. the solution of the navier-stokes equation"""
+def _get_fr_coeffs(X):
     s2 = np.sqrt(2)
-    x = r / r0
-    return v0 * x + ((1+s2)/2 * x**s2 + (1-s2)/2 / x**s2 - x**2) * (g*r0**2)/(2*nu) * np.cos(phi)
-
-def get_velocity(r, phi, v0, r0, nu, R0):
-    """Calculate the velocity profile, i.e. the flow velocity in tangential direction for an arbitrary r and phi.
-    i.e. the solution of the navier-stokes equation"""
-    s2 = np.sqrt(2)
-    x = r / r0
-    X0 = R0 / r0
-
-
-    A0 = 1/(1+X0**2)
+    A0 = 1/(1+X**2)
     B0 = 1 - A0
-    A1 = (s2 * X0**2 + X0**(-s2)) / (X0**s2 + X0**(-s2))#(1/s2 + X0**(-s2)) / (X0**s2 + X0**(-s2))
+    A1 = (s2 * X**2 + X**(-s2)) / (X**s2 + X**(-s2))
     B1 = 1 - A1
+    return A0, B0, A1, B1
 
+
+def get_velocity(r, phi, v0, r0, nu, R):
+    """Calculate the velocity profile, i.e. the flow velocity in tangential direction for an arbitrary r and phi.
+    i.e. the solution of the navier-stokes equation"""
+    s2 = np.sqrt(2)
+    x = r / r0
+    A0, B0, A1, B1 = _get_fr_coeffs(R/r0)
     return v0 * (A0*x + B0/x) - ( (A1 * x**s2) + (B1 * x**(-s2)) - x**2) * (g*r0**2)/(2*nu) * np.cos(phi)
+
+
+def get_flow(phi, v0, r0, nu, R):
+    s2 = np.sqrt(2)
+    X = R/r0
+    A0, B0, A1, B1 = _get_fr_coeffs(X)
+
+    A = A0*(X**2-1)/2 + B0*np.log(X)
+    B = A1 * (X**(1+s2)-1) / (1+s2) + B1 * (X**(1-s2)-1) / (1-s2) - (X**3-1)/3
+    v_tilde = (g*r0**2)/(2*nu)
+    return v0 * A - B * v_tilde * np.cos(phi)
 
 
 def _flow_root_func(x, phi, v0, r0, nu, R0):
@@ -36,17 +42,18 @@ def _flow_root_func(x, phi, v0, r0, nu, R0):
     return A + B*C - flow
 
 
-def _get_R_outer(phi, v0, r0, nu, R0):
+def _get_R_outer(phi, v0, r0, nu, Rmax):
     """Calculate the outer perimeter of the honey-blob as a function of phi, using conservation of flow"""
-    res = root(_flow_root_func, 1.0, args=(phi, v0, r0, nu, R0))
+    flow0 = get_flow(np.pi/2, v0, r0, nu, Rmax)
+    res = root(lambda R: get_flow(phi, v0, r0, nu, R) - flow0, r0)
     if res.success:
-        return res.x[0] * r0
+        return res.x[0]
     else:
         return np.nan
 
-def get_R_outer(phi, v0, r0, nu, R0):
+def get_R_outer(phi, v0, r0, nu, R):
     try:
-        return np.array([_get_R_outer(phi_, v0, r0, nu, R0) for phi_ in phi])
+        return np.array([_get_R_outer(phi_, v0, r0, nu, R) for phi_ in phi])
     except TypeError:
-        return _get_R_outer(phi, v0, r0, nu, R0)
+        return _get_R_outer(phi, v0, r0, nu, R)
 